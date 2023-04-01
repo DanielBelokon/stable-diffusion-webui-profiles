4c102ea0f3559084e5e15d88be8986b80e23851c
Danny <daniel.belokon@gmail.com>
2023-04-01 15:53:03 +0300

    Create state and profiles classes
    Use classes instead of randomly throwing functions around
    Add a state class to abstract away the state file/object and profile paths

--- scripts/profile_state.py
+++ scripts/profile_state.py
@@ -1,6 +1,38 @@
 import modules
 import os
+import json
 
 
-profiles_settings_path = os.path.join(modules.scripts.basedir(), "profiles_settings.json")
-profiles_settings_data = {"profile": "config.json", "profile_list": ["config.json"]}
+class ProfileState:
+    settings_path = os.path.join(modules.scripts.basedir(), "profiles_settings.json")
+    data = {"profile": "config.json", "profile_list": ["config.json"]}
+
+    def save(self):
+        with open(self.settings_path, 'w') as ps_fd:
+            json.dump(self.data, ps_fd)
+
+    def load(self):
+        if os.path.exists(self.settings_path):
+            with open(self.settings_path, 'r') as ps_fd:
+                self.data = json.load(ps_fd)
+
+    def profile_path(self, profile):
+        return profile
+
+    def list(self):
+        return self.data["profile_list"]
+
+    def add(self, profile_name):
+        if profile_name not in self.data["profile_list"]:
+            self.data["profile_list"].append(profile_name)
+            self.save()
+
+    def current(self):
+        return self.data["profile"]
+
+    def current_path(self):
+        return self.profile_path(self.current())
+
+    def set_current(self, profile):
+        self.data["profile"] = profile
+
--- scripts/profiles.py
+++ scripts/profiles.py
@@ -6,117 +6,109 @@ import gradio as gr
 from modules import shared
 from modules.shared import opts, OptionInfo
 from modules import scripts
-from scripts.profile_state import profiles_settings_data, profiles_settings_path
+from scripts.profile_state import ProfileState
 
 
-def on_ui_settings():
-    section = ("profiles", "Profiles")
+class ConfigProfiles:
 
+    def __init__(self):
+        self.ps = ProfileState()
 
-def profile_update(profile_name):
-    if not os.path.exists(profile_name):
-        print("Profile doesn't exist - cloning current config...")
-        profile_add(profile_name)
+    def on_ui_settings(self):
+        section = ("profiles", "Profiles")
 
-    else:
-        opts.load(profile_name)
-
-    shared.config_filename = profile_name
-    profiles_settings_data["profile"] = profile_name
-    with open(profiles_settings_path, 'w') as file:
-        json.dump(profiles_settings_data, file)
-
-    shared.state.interrupt()
-    shared.state.need_restart = True
-
-
-def profile_add(profile_name):
-    if profile_name == "":
-        print("Config Name can't be empty")
-    else:
+    def profile_update(self, profile_name):
         if not os.path.exists(profile_name):
-            opts.save(profile_name)
+            self.ps.add(profile_name)
+
+        self.ps.set_current(profile_name)
+
+        # Load new options file and set the new config "filename" (used for saving elsewhere)
+        opts.load(self.ps.current_path())
+        shared.config_filename = self.ps.current_path()
+
+        self.ps.save()
+
+        # Restart webui
+        shared.state.interrupt()
+        shared.state.need_restart = True
+
+    def profile_add(self, profile_name):
+        if profile_name == "":
+            print("Config Name can't be empty")
         else:
-            print("Found existing profile, updating list...")
-        if profile_name not in profiles_settings_data["profile_list"]:
-            profiles_settings_data["profile_list"].append(profile_name)
-            with open(profiles_settings_path, 'w') as file:
-                json.dump(profiles_settings_data, file)
+            if not os.path.exists(profile_name):
+                opts.save(profile_name)
+            else:
+                print("Found existing profile, updating list...")
 
-    return gr.Radio.update(choices=profiles_settings_data["profile_list"])
+            self.ps.add(profile_name)
 
+        return gr.Radio.update(choices=self.ps.list())
 
-def initialize_profile():
-    global profiles_settings_data
-    if os.path.exists(profiles_settings_path):
-        with open(profiles_settings_path, "r", encoding="utf8") as profiles_settings_file:
-            print("Preloading profile settings... ")
-            profiles_settings_data = json.load(profiles_settings_file)
+    def initialize_profile(self):
+        self.ps.load()
+        print("Profile set to " + self.ps.current())
+        shared.config_filename = self.ps.current_path()
+        opts.load(shared.config_filename)
 
-    print("Profile set to " + profiles_settings_data["profile"])
-    shared.config_filename = profiles_settings_data["profile"]
-    opts.load(shared.config_filename)
+    def change_preview(self, profile):
+        configfile = []
+        with open(profile, 'r', encoding="utf8") as file:
+            configfile = json.load(file)
 
+        return gr.Json.update(value=configfile)
 
-def change_preview(profile):
-    configfile = []
-    with open(profile, 'r', encoding="utf8") as file:
-        configfile = json.load(file)
+    def add_tab(self):
+        self.ps.load()
 
-    return gr.Json.update(value=configfile)
+        if self.ps.data:
+            profile_list = self.ps.list()
+            cur_profile = self.ps.current()
+        else:
+            profile_list = ["config.json"]
+            cur_profile = "config.json"
 
+        with open(self.ps.profile_path(cur_profile), 'r', encoding="utf8") as file:
+            configfile = json.load(file)
 
-def add_tab():
-    global profiles_settings_data
-    if os.path.exists(profiles_settings_path):
-        with open(profiles_settings_path, "r", encoding="utf8") as profiles_settings_file:
-            profiles_settings_data = json.load(profiles_settings_file)
+        with gr.Blocks(analytics_enabled=False) as tab:
+            gr.Markdown("# Config Profiles ")
+            gr.Markdown("### Current Profile: " + cur_profile)
+            with gr.Column(variant='panel', scale=1):
+                with gr.Row(variant='panel').style(equal_height=True):
+                    with gr.Row():
+                        profile_radio = gr.Radio(label="Profiles", choices=profile_list, value=cur_profile)
+                        apply_profile = gr.Button("Apply", elem_id="profile_apply", variant='primary')
+                with gr.Row(variant='panel').style(equal_height=True):
+                    with gr.Row():
+                        new_profile_input = gr.Textbox("", placeholder="new-config.json", label="New Profile Name")
+                        # new_profile_base = gr.Radio(label="Profiles", choices=["Selected", "Current", "Default"], value="Current")
+                        add_profile = gr.Button("Add", elem_id="profile_add")
+            with gr.Column(scale=3):
+                profile_display = gr.Json(value=configfile)
 
-    if profiles_settings_data:
-        profile_list = profiles_settings_data["profile_list"]
-        cur_profile = profiles_settings_data["profile"]
-    else:
-        profile_list = ["config.json"]
-        cur_profile = "config.json"
+            apply_profile.click(
+                fn=self.profile_update,
+                _js="reload_ui_profile",
+                inputs=[profile_radio],
+                outputs=[]
+            )
 
-    with open(cur_profile, 'r', encoding="utf8") as file:
-        configfile = json.load(file)
+            add_profile.click(
+                fn=self.profile_add,
+                inputs=[new_profile_input],
+                outputs=[profile_radio]
+            )
 
-    with gr.Blocks(analytics_enabled=False) as tab:
-        gr.Markdown("# Config Profiles ")
-        gr.Markdown("### Current Profile: " + cur_profile)
-        with gr.Column(variant='panel', scale=1):
-            with gr.Row(variant='panel').style(equal_height=True):
-                with gr.Row():
-                    profile_radio = gr.Radio(label="Profiles", choices=profile_list, value=cur_profile)
-                    apply_profile = gr.Button("Apply", elem_id="profile_apply", variant='primary')
-            with gr.Row(variant='panel').style(equal_height=True):
-                with gr.Row():
-                    new_profile_input = gr.Textbox("", placeholder="new-config.json", label="New Profile Name")
-                    # new_profile_base = gr.Radio(label="Profiles", choices=["Selected", "Current", "Default"], value="Current")
-                    add_profile = gr.Button("Add", elem_id="profile_add")
-        with gr.Column(scale=3):
-            profile_display = gr.Json(value=configfile)
+            profile_radio.change(fn=self.change_preview, inputs=profile_radio, outputs=profile_display)
 
-        apply_profile.click(
-            fn=profile_update,
-            _js="reload_ui_profile",
-            inputs=[profile_radio],
-            outputs=[]
-        )
-
-        add_profile.click(
-            fn=profile_add,
-            inputs=[new_profile_input],
-            outputs=[profile_radio]
-        )
-
-        profile_radio.change(fn=change_preview, inputs=profile_radio, outputs=profile_display)
-
-    return [(tab, "Profiles", "profiles")]
+        return [(tab, "Profiles", "profiles")]
 
 
-scripts.script_callbacks.on_ui_tabs(add_tab)
+conprof = ConfigProfiles()
+
+scripts.script_callbacks.on_ui_tabs(conprof.add_tab)
 # scripts.script_callbacks.on_ui_settings(on_ui_settings)
-scripts.script_callbacks.on_before_ui(initialize_profile)
+scripts.script_callbacks.on_before_ui(conprof.initialize_profile)
 
