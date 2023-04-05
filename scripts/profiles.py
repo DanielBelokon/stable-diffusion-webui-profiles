865f3aa9bd2ce3d2155062f33216ee48cf176256
Danny <daniel.belokon@gmail.com>
2023-04-05 15:26:52 +0300

    Enabled settings editing from the Profiles tab

--- scripts/profiles.py
+++ scripts/profiles.py
@@ -13,9 +13,8 @@ class ConfigProfiles:
 
     def __init__(self):
         self.ps = ProfileState()
-
-    def on_ui_settings(self):
-        section = ("profiles", "Profiles")
+        self.display_options = shared.Options()
+        self.settings_components = {}
 
     def profile_update(self, profile_name):
         if not os.path.exists(profile_name):
@@ -84,16 +83,101 @@ class ConfigProfiles:
         else:
             opts.save(self.ps.current_path())
 
-    def change_preview(self, profile):
-        if os.path.exists(self.ps.profile_path(profile)):
-            with open(self.ps.profile_path(profile), 'r', encoding="utf8") as file:
-                configfile = json.load(file)
-        else:
-            configfile = {}
-            print("Current config not found!")
-            self.ps.remove(profile)
+# *********************************************
+#               Display stuff
+# *********************************************
+    def save_profile_config(self, profile):
+        print("Saving to " + self.ps.profile_path(profile))
+        self.display_options.save(self.ps.profile_path(profile))
 
-        return gr.Json.update(value=configfile)
+    def set_display_setting(self, key, value):
+        print("changed" + str(key) + " to " + str(value))
+        self.display_options.set(key, value)
+
+    def create_settings_display(self):
+        previous_section = None
+        current_tab = None
+        current_row = None
+        gr.Markdown(value="# These settings will only apply to the selected profile ")
+
+        # Basically copied from ui.py, creates settings tab with current selected profile (display_options) data
+        with gr.Tabs():
+            for i, (key, item) in enumerate(self.display_options.data_labels.items()):
+                section_must_be_skipped = item.section[0] is None
+                if previous_section != item.section and not section_must_be_skipped:
+                    elem_id, text = item.section
+
+                    if current_tab is not None:
+                        current_row.__exit__()
+                        current_tab.__exit__()
+
+                    gr.Group()
+                    current_tab = gr.TabItem(elem_id="p_settings_{}".format(elem_id), label=text, open=False)
+                    current_tab.__enter__()
+                    current_row = gr.Column(variant='compact')
+                    current_row.__enter__()
+
+                    previous_section = item.section
+
+                if not section_must_be_skipped:
+                    info = self.display_options.data_labels[key]
+                    t = type(info.default)
+
+                    args = info.component_args() if callable(info.component_args) else info.component_args
+
+                    if info.component is not None:
+                        comp = info.component
+                    elif t == str:
+                        comp = gr.Textbox
+                    elif t == int:
+                        comp = gr.Number
+                    elif t == bool:
+                        comp = gr.Checkbox
+                    else:
+                        raise Exception(f'bad options item type: {str(t)} for key {key}')
+
+                    elem_id = "p_setting_" + key
+                    with gr.Row():
+                        key_comp = gr.Textbox(visible=False, value=key)
+                        value = self.display_options.data[key] if key in self.display_options.data else self.display_options.data_labels[key].default
+                        self.settings_components[key] = comp(label=info.label, value=value, elem_id=elem_id, **(args or {}))
+                        self.settings_components[key].change(fn=self.set_display_setting, inputs=[key_comp, self.settings_components[key]], outputs=[])
+            if current_tab is not None:
+                current_row.__exit__()
+                current_tab.__exit__()
+            gr.Group()
+            current_tab = gr.TabItem(elem_id="p_settings_extensions", label="Extensions", open=False)
+            current_tab.__enter__()
+            current_row = gr.Column(variant='compact')
+            current_row.__enter__()
+
+            # Create and populate extensions tab
+            with gr.Row():
+                from modules import extensions
+                self.settings_components["enabled_extensions"] = gr.CheckboxGroup(choices=[ext.name for ext in extensions.extensions],
+                                                                                  value=[ext.name for ext in extensions.extensions if ext.name not in self.display_options.disabled_extensions],
+                                                                                  label="Enabled Extensions")
+
+            self.settings_components["enabled_extensions"].change(
+                fn=lambda en_list: self.display_options.set("disabled_extensions", [ext for ext in self.settings_components["enabled_extensions"].choices if ext not in en_list]),
+                inputs=[self.settings_components["enabled_extensions"]],
+                outputs=[]
+            )
+
+    def display_update_components(self, profile):
+        self.display_options.load(self.ps.profile_path(profile))
+        updated_components_fucking_gradio = []
+        for key, comp in self.settings_components.items():
+            # "disabled extensions" saved instead (for whatever reason?), we do it outside the loop
+            if key == "enabled_extensions":
+                continue
+            updated_components_fucking_gradio.append(comp.update(value=self.display_options.data[key] if key in self.display_options.data else self.display_options.data_labels[key].default))
+
+
+        from modules import extensions
+        updated_components_fucking_gradio.append(gr.CheckboxGroup.update(value=[ext.name for ext in extensions.extensions if ext.name not in self.display_options.disabled_extensions]))
+
+        return updated_components_fucking_gradio
 
     def add_tab(self):
         self.ps.load()
@@ -107,6 +191,7 @@ class ConfigProfiles:
         if os.path.exists(self.ps.current_path()):
             with open(self.ps.current_path(), 'r', encoding="utf8") as file:
                 configfile = json.load(file)
+                self.display_options.load(self.ps.current_path())
         else:
             configfile = []
             print("Current config not found!")
@@ -123,7 +208,7 @@ class ConfigProfiles:
                     gr.Markdown("## Switch and inspect profiles ")
                     with gr.Row(variant='panel'):
                         profile_radio = gr.Radio(label="Profiles", choices=profile_list, value=cur_profile)
-                        apply_profile = gr.Button("Apply", elem_id="profile_apply", variant='primary')
+                        apply_profile = gr.Button("Load selected profile", elem_id="profile_apply", variant='primary')
                     gr.Markdown("## Add new profiles ")
                     with gr.Row(variant='panel'):
                         with gr.Row():
@@ -131,15 +216,20 @@ class ConfigProfiles:
                             image_output = gr.Textbox("outputs", placeholder="Leave empty for current profile's dir",
                                                       label="Profile's root Image outputs folder")
 
-                        add_profile = gr.Button("Add", elem_id="profile_add")
+                        add_profile = gr.Button("Create", elem_id="profile_add")
 
-                    with gr.Row(variant='panel'):
-                        with gr.Accordion(label="Delete Profiles", open=False):
-                            delete_profile_radio = gr.Radio(label="Profiles", choices=profile_list)
-                            delete_profile_button = gr.Button("DELETE", elem_id="profile_delete", variant='stop')
+                    with gr.Accordion(label="Edit Selected Profile", open=False):
+                        settings_submit = gr.Button(value="Save", variant='primary', elem_id="p_settings_submit")
+                        self.create_settings_display()
 
-                with gr.Column(variant='panel', scale=1):
-                    profile_display = gr.Json(value=configfile)
+                    with gr.Accordion(label="Delete Profiles", open=False):
+                        delete_profile_radio = gr.Radio(label="Profiles", choices=profile_list)
+                        delete_profile_button = gr.Button("DELETE", elem_id="profile_delete", variant='stop')
+
+            settings_submit.click(
+                fn=self.save_profile_config,
+                inputs=[profile_radio], outputs=[]
+            )
 
             apply_profile.click(
                 fn=self.profile_update,
@@ -160,7 +250,8 @@ class ConfigProfiles:
                 outputs=[profile_radio, delete_profile_radio]
             )
 
-            profile_radio.change(fn=self.change_preview, inputs=profile_radio, outputs=profile_display)
+
+            profile_radio.change(fn=self.display_update_components, inputs=profile_radio, outputs=list(self.settings_components.values()))
 
         return [(tab, "Profiles", "profiles")]
 
@@ -168,6 +259,5 @@ class ConfigProfiles:
 conprof = ConfigProfiles()
 
 scripts.script_callbacks.on_ui_tabs(conprof.add_tab)
-# scripts.script_callbacks.on_ui_settings(on_ui_settings)
 scripts.script_callbacks.on_before_ui(conprof.initialize_profile)
 
